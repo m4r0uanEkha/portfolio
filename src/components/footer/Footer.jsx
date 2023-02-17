a8895552f6eb5d6cf27c071ad181402631586e12
m4r0uanEkha <123416126+m4r0uanEkha@users.noreply.github.com>
2023-02-17 18:34:36 +0100

    Footer icons updated

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import "./footer.css"
 import { BsLinkedin, BsTwitter, BsInstagram, BsFacebook } from "react-icons/bs"
+import { FaTwitterSquare, FaInstagramSquare, FaFacebookSquare } from "react-icons/fa"
 
 const Footer = (props) => {
     return (
         <div className={`footer-container${props.darkTheme ? " dark" : ""}`}>
             <div className="links">
                 <a href='https://www.linkedin.com/in/marouane-khatar/' target={'_blank'}><BsLinkedin /></a>
-                <a href='https://twitter.com/' target={'_blank'}><BsTwitter /></a>
-                <a href='https://www.instagram.com/' target={'_blank'}><BsInstagram /></a>
-                <a href='https://www.facebook.com/' target={'_blank'}><BsFacebook /></a>
+                <a href='https://twitter.com/' target={'_blank'}><FaTwitterSquare /></a>
+                <a href='https://www.instagram.com/' target={'_blank'}><FaInstagramSquare /></a>
+                <a href='https://www.facebook.com/' target={'_blank'}><FaFacebookSquare /></a>
             </div>
             <small>Made by Marouane Khatar</small>
         </div>
