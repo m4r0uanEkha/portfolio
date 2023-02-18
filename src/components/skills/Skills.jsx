65736382086088a631ac0e5ea688e022ade1a361
m4r0uanEkha <123416126+m4r0uanEkha@users.noreply.github.com>
2023-02-18 08:54:12 +0100

    Bubble updated

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import "./skills.css"
 
 import Picture from "../../assets/profil.jpg"
+import Bubble from './Bubble'
 
 const Skills = (props) => {
   return (
@@ -9,14 +10,24 @@ const Skills = (props) => {
           <h5>- Skills -</h5>
           <img src={Picture} alt="profil" className='skills-img' />
           <div className="skills-grid">
-              <div className="skills-bubble">Design engineering</div>
+              <Bubble text="Design engineering" />
+              <Bubble text="Application development" />
+              <Bubble text="SCRUM" />
+              <Bubble text="Javascript" />
+              <Bubble text="ReactJS" />
+              <Bubble text="Agile methodology" />
+              <Bubble text="Android" />
+              <Bubble text="Python" />
+
+
+              {/* <div className="skills-bubble">Design engineering<div className="triangle"></div></div>
               <div className="skills-bubble">Application development</div>
               <div className="skills-bubble">SCRUM</div>
               <div className="skills-bubble">Javascript</div>
               <div className="skills-bubble">ReactJS</div>
               <div className="skills-bubble">Agile methodology</div>
               <div className="skills-bubble">Android</div>
-              <div className="skills-bubble">Python</div>
+              <div className="skills-bubble">Python</div> */}
           </div>
       </div>
   )
